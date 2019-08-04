a43b4731f3e7bd307014e4904fbfe89918aca948
Sergey Goshkov <gsn82@mail.ru>
2019-08-04 18:17:23 +0500

    lessson 6

--- src/main/java/ru/gorshkov/springcourse/MusicPlayer.java
+++ src/main/java/ru/gorshkov/springcourse/MusicPlayer.java
@@ -3,10 +3,40 @@ package ru.gorshkov.springcourse;
 public class MusicPlayer {
     private Music music;
 
+    private String name;
+    private int volume;
+
     //IoC
     // инверсия управления
     public MusicPlayer(Music music) {
         this.music = music;
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public MusicPlayer() {
     }
 
     //выводим песню на экран
